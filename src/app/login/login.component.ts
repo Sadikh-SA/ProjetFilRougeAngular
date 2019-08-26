cd7fbcaba38c56ae332893e5c874626e475b1de8
Sadikh-SA <abougueye96@yahoo.fr>
2019-08-26 15:54:53 +0000

    Création du composant login

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  connexionUser = FormGroup
+  constructor() { }
+
+  ngOnInit() {
+  }
+  connexion() {
+    console.log(this.connexionUser);
+  }
+
+}
